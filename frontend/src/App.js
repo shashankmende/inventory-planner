9abf3a2a5e7460449fea1990de5544be8d7f4ada
Shashank@eruvaka <shashank.mende@eruvaka.com>
2026-08-13 11:25:39 +0530

    refactor: simplify App component structure by removing Home route and integrating AppRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-function Home() {
-  const [apiStatus, setApiStatus] = React.useState('');
-  React.useEffect(() => {
-    // fetch('http://localhost:4000/health')
-    fetch('https://inventory-planner-api.onrender.com/health')
-      .then(res => res.json())
-      .then(data => setApiStatus(data.status))
-      .catch(() => setApiStatus('API not reachable'));
-  }, []);
-  return (
-    <div>
-      <h1>Inventory Planner</h1>
-      <p>Backend status: {apiStatus}</p>
-    </div>
-  );
-}
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AppProvider } from './app/AppContext';
+import AppRouter from './app/router';
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <AppProvider>
+        <AppRouter />
+      </AppProvider>
     </Router>
   );
 }
